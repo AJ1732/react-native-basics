e2c91ef2bd8ccf9ba09a857d0758094575d36f7c
AJ1732 <ejemeniboi@gmail.com>
2025-09-10 01:47:28 +0100

    init: hello react native

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { StyleSheet, Text, View } from "react-native";
+
+const Home = () => {
+  return (
+    <View className="bg-orange-50 flex flex-col flex-1 items-center justify-center">
+      <Text className="text-orange-600 text-xl font-bold">Hello World</Text>
+    </View>
+  );
+};
+export default Home;
+const styles = StyleSheet.create({});
